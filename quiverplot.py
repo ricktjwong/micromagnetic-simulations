8d8ba49f2511c9db5fc2ef68779d3a813ae50ae0
ricktjwong <rickwtj@gmail.com>
2019-11-01 12:03:16 +0000

    Edit mumax strayfield

--- quiverplot.py
+++ quiverplot.py
@@ -11,7 +11,7 @@ plt.rcParams['mathtext.default'] = 'regular'
 
 
 def plot_3D(n: int):
-    # Convert from A/m to mT (1 A/m -> 0.000001254 T)
+    # Convert from A/m to T (1 A/m -> 0.000001254 T)
     data = np.array(np.loadtxt('./single/rect_single_cobalt.txt')) / 10000
 
     # The order here is u, v, w = (40, 140, 24)
@@ -53,7 +53,7 @@ def plot_2D_quiver(file_path: str, zslice: int):
     # rect2 = patches.Rectangle((70, 30), 24, 120, linewidth=1, edgecolor='r', facecolor='none')
     # ax.add_patch(rect1)
     # ax.add_patch(rect2)
-    # plt.savefig(file_path.split('/')[-1].split('.')[0] + '.pdf', dpi=1000)
+    plt.savefig(file_path.split('/')[-1].split('.')[0] + '.pdf', dpi=1000)
     plt.show()
 
 
@@ -86,8 +86,11 @@ def get_meta_data(file_path: str):
 # plot_2D_quiver(file_path="./data/stray_field/processed/double_100_x_120/strayfield_updown_doubleAsym_100_200_120.ovf", zslice=0)
 # plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_100_100_100.ovf", zslice=0)
 # plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_100_150_100.ovf", zslice=0)
+
+plot_2D_quiver(file_path="./data/stray_field/processed/permalloy_6array.txt", zslice=0)
+
 # plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_100_200_100.ovf", zslice=0)
-plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_staggered_100_200_100.ovf", zslice=0)
+# plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_staggered_100_200_100.ovf", zslice=0)
 # plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_staggered_100_150_100.ovf", zslice=0)
 # plot_2D_quiver(file_path="./data/stray_field/processed/two_rows/strayfield_2rows_staggered_100_200_100.ovf", zslice=0)
 
