a1a491e0612ab3e898939e3dbd0d21bf8024d0b8
ricktjwong <rickwtj@gmail.com>
2020-01-03 15:06:06 +0000

    Test halbach cylinder

--- scripts/generate_bitmap.sh
+++ scripts/generate_bitmap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-tclsh /Users/ricktjwong/oommf/oommf.tcl avf2ppm -format B24 -config ../avf2ppm.config ../data/stray_field/permalloy/strayfield_double_100_x_150/*.ovf
+tclsh /Users/ricktjwong/oommf/oommf.tcl avf2ppm -format B24 -config ../avf2ppm.config ../cobalt-halbach-2rows/halbach2rows.0.out/m000000.ovf
+tclsh /Users/ricktjwong/oommf/oommf.tcl avf2ppm -format B24 -config ../avf2ppm.config ../cobalt-halbach-2rows/halbach2rows.0.out/m000001.ovf
