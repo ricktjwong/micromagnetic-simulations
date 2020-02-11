6248672a97da7fd05484aadbae6da98369de19af
ricktjwong <rickwtj@gmail.com>
2020-02-11 14:49:12 +0000

    Generalise empty space for optimise script

--- optimisation/optimise.py
+++ optimisation/optimise.py
@@ -14,19 +14,21 @@ def get_meta_data(file_path: str):
     headers = {}
     with open(file_path) as f:
         for line in f:
-            if line[0] != '#': break
+            if line[0] != '#':
+                break
             if ':' in line:
                 key_value = line.split('# ')[1].split(':')
                 key, value = key_value[0], key_value[1].split('\n')[0].strip()
                 headers[key] = value
-    return int(headers['xnodes']), int(headers['ynodes']), int(headers['znodes']), \
-           float(headers['xstepsize']), float(headers['ystepsize']), float(headers['zstepsize'])
+    return int(headers['xnodes']), int(headers['ynodes']), \
+           int(headers['znodes']), float(headers['xstepsize']), \
+           float(headers['ystepsize']), float(headers['zstepsize'])
 
 
-def initialise_gridspace(x0: [int], filename: str, x: int, y: int):
+def initialise_gridspace(x0: [int], filename: str,
+                         x: int, y: int, empty_space: [int]):
     configs = ['uniform(0, 0, 0)', 'uniform(0, 1, 0)', 'uniform(0, -1, 0)',
                'uniform(1, 0, 0)', 'uniform(-1, 0, 0)']
-    empty_space = [i for i in range(1, x*y + 1, int(y/2))]
     with open('./boilerplate' + str(x) + 'x' + str(y) + '.mx3') as f:
         with open('./mumax_scripts/' + filename, 'w') as f1:
             for line in f:
@@ -35,9 +37,10 @@ def initialise_gridspace(x0: [int], filename: str, x: int, y: int):
             # Exclude indices that are not part of the empty space
             for i in range(1, x*y+1):
                 if i not in empty_space:
-                    lines += 'm.SetRegion(' + str(i) + ', ' + configs[x0[i-1]] + ')\n'
-                    # If the cell is initialised empty, set it as an actual empty cell
-                    # with no magnetic saturation (as in vacuum)
+                    lines += 'm.SetRegion(' + str(i) + ', ' \
+                          + configs[x0[i-1]] + ')\n'
+                    # If the cell is initialised empty, set it as an actual
+                    # empty cell with no magnetic saturation (as in vacuum)
                     if x0[i-1] == 0:
                         lines += 'Msat.SetRegion(' + str(i) + ', 0)\n'
                         lines += 'Aex.SetRegion(' + str(i) + ', 0)\n'
@@ -49,7 +52,8 @@ def initialise_gridspace(x0: [int], filename: str, x: int, y: int):
 
 
 def find_max_B(file_path: str):
-    ovf_path = './mumax_scripts/' + file_path + '.out/' + 'strayfield_optimise.ovf'
+    ovf_path = './mumax_scripts/' + file_path + '.out/' + \
+               'strayfield_optimise.ovf'
     x, y, z, _, _, _ = get_meta_data(ovf_path)
     zslice = int(z / 2)
     yslice = int(y / 2)
@@ -57,7 +61,7 @@ def find_max_B(file_path: str):
     data_field = data.reshape(x, y, z, 3, order='F')
     u, v, w = data_field[:, :, :, 0], data_field[:, :, :, 1], data_field[:, :, :, 2]
     mag = (u * u + v * v + w * w) ** 0.5
-    mag_slice = mag[:, yslice, zslice]
+    mag_slice = mag[int(x/3):int(x/3)*2, yslice, zslice]
     return max(mag_slice)
 
 
@@ -76,11 +80,11 @@ def acceptance_probability(old, new, T):
     return a
 
 
-def simulated_annealing(x0, T, T_min, alpha, x: int, y: int):
+def simulated_annealing(x0: [int], T: float, T_min: float, alpha: float,
+                        x: int, y: int, empty_space: [int]):
     max_costs = []
     max_actions = []
     max_cost = 0.0
-    empty_space = [i for i in range(1, x*y + 1, int(y/2))]
     x_new = x0.copy()
     if not os.path.exists('./mumax_scripts'):
         os.makedirs('mumax_scripts')
@@ -90,9 +94,10 @@ def simulated_annealing(x0, T, T_min, alpha, x: int, y: int):
         count = 0
         while(count < 100):
             filename = str(T).replace('.', '') + '_' + str(count) + '.mx3'
-            initialise_gridspace(x_new, filename, x, y)
+            initialise_gridspace(x_new, filename, x, y, empty_space)
             run_mumax_script(filename)
-            while not os.path.exists('./mumax_scripts/' + filename.split('.')[0] + '.out'):
+            while not os.path.exists('./mumax_scripts/' +
+                                     filename.split('.')[0] + '.out'):
                 time.sleep(5)
             cost_new = find_max_B(filename.split('.')[0])
             print('x0: ', x0)
@@ -125,13 +130,12 @@ def simulated_annealing(x0, T, T_min, alpha, x: int, y: int):
 
 
 x, y = 6, 6
+empty_space_two_rows = [i for i in range(1, x*y + 1, int(y/2))]
+empty_space_box_6x6 = [1, 10, 19, 28]
+empty_space_box_12x12 = [1, 2, 7, 8, 37, 38, 43, 44, 73, 74,
+                         79, 80, 109, 110, 115, 116]
 x0 = [0 for i in range(x*y)]
-# x0 = '0 1 4 1 0 4 0 0 2 2 3 3 3 2 0 1 1 2 2 4 3 0 3 4 4 3 3 1 0 1 4 1 2 4 4 0 3 2 3 3 1 1 0 2 3 2 4 0 2 0 3 1 3 3 2 0 0 3 2 4 3 1 2 0 3 3 3 4 4 2 0 1 4 4 2 2 3 0 4 4 4 1 3 3 0 1 4 2 1 2 4 0 4 1 4 2 4 2 0 3 1 4 1 4 4 0 1 4 3 1 2 0 0 2 1 4 4 4 4 0 1 1 3 2 3 2 0 2 4 4 2 1 4 0 2 1 1 2 2 4 0 3 2 1 1 2 3 0 2 3 3 3 4 3 0 4 3 4 3 2 0 0 3 1 2 2 1 4 0 1 3 3 3 1 3 0 3 2 2 3 4 2 0 3 2 4 4 4 3 0 3 3 1 1 4 4'
-# x0 = x0.split(' ')
-# x0 = [int(i) for i in x0]
-# print(x0)
-# print(len(x0))
 T = 1.0
 T_min = 0.00001
 alpha = 0.8
-simulated_annealing(x0, T, T_min, alpha, x, y)
+simulated_annealing(x0, T, T_min, alpha, x, y, empty_space_box_6x6)
