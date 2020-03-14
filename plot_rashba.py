2acf43cb3ba6810ced7f053f3b434c3d89f75754
ricktjwong <rickwtj@gmail.com>
2020-03-14 16:26:40 +0000

    Refactor code
    - Place rashba plots in a separate file
    - Create function to extract max B from sweeps

---
+++ plot_rashba.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_meta_data(file_path: str):
+    headers = {}
+    with open(file_path) as f:
+        for line in f:
+            if line[0] != '#': break
+            if ':' in line:
+                key_value = line.split('# ')[1].split(':')
+                key, value = key_value[0], key_value[1].split('\n')[0].strip()
+                headers[key] = value
+    return int(headers['xnodes']), int(headers['ynodes']), int(headers['znodes']), \
+           float(headers['xstepsize']), float(headers['ystepsize']), float(headers['zstepsize'])
+
+
+def plot_rashba(file_path: str, yslice: [int]):
+    eff_m = 0.014
+    x, y, z, _, _, _ = get_meta_data(file_path)
+    print(x, y, z)
+    zslice = int(z / 2)
+    data = np.array(np.loadtxt(file_path))
+    data_field = data.reshape(x, y, z, 3, order="F")
+    u, v, w = data_field[:, :, :, 0], data_field[:, :, :, 1], \
+        data_field[:, :, :, 2]
+    fig, ax1 = plt.subplots()
+    ax1.set_ylabel("alpha (eVm)")
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("Magnetic Field (T)")
+    # ax1.set_ylim([1e-12, 5e-11])
+    # ax2.set_ylim([0, 0.07])
+    for i in yslice:
+        Bx = u[:, i, zslice]
+        By = v[:, i, zslice]
+        B = (u*u + v*v + w*w) ** 0.5
+        B = B[:, i, zslice]
+        grad_B = np.gradient(B)
+        grad_Bx = np.gradient(Bx)
+        # assumes in nanometres
+        dphi_dx = -(Bx * grad_B - B * grad_Bx)/(B*By*2*np.pi)
+        # alt expression but uses sqrt, might have sign issues?
+        dphi_dx2 = (Bx * grad_B - B * grad_Bx)/(2*np.pi*B*B*np.sqrt(1-Bx*Bx/(B*B)))
+        # extra 2pi factor from X = 2(pi)x
+        alpha = (3.818E-11/eff_m) * dphi_dx  # in eVm ## first factor hbar^2/2em, divide by e for eVs
+        alpha2 = (3.818E-11/eff_m) * dphi_dx2
+        ax1.plot([i * 5 for i in range(x)], alpha, label="alpha", color="blue")
+        # ax1.plot([i * 5 for i in range(x)], alpha2, color="orange", linestyle = "--")
+        # ax2.plot([i * 5 for i in range(x)], Bx,label= "$B_x$", color="brown")
+        # ax2.plot([i * 5 for i in range(x)], By,label="$B_y$", color="green")
+        ax2.plot([i * 5 for i in range(x)], B, label="$|B|$",  color="red")
+        # ax2.plot([i * 5 for i in range(x)], Bx/B, label="$Bx/|B|$",  color="red")
+        plt.legend()
+        plt.show()
+    return alpha
+
+
+def surface_alpha(file_path: str, yrange: tuple):
+    eff_m = 0.014
+    x, y, z, _, _, _ = get_meta_data(file_path)
+    print(x, y, z)
+    zslice = int(z / 2)
+    data = np.array(np.loadtxt(file_path))
+    data_field = data.reshape(x, y, z, 3, order="F")
+    u, v, w = data_field[:, :, :, 0], data_field[:, :, :, 1], \
+        data_field[:, :, :, 2]
+    for i in range(yrange[0], yrange[1]):
+        Bx = u[:, i, zslice]
+        By = v[:, i, zslice]
+        B = (u*u + v*v + w*w) ** 0.5
+        B = B[:, i, zslice]
+        grad_B = np.gradient(B)
+        grad_Bx = np.gradient(Bx)
+        dphi_dx = -(Bx * grad_B - B * grad_Bx)/(B*By*2*np.pi)
+        alpha = (3.818E-11/eff_m) * dphi_dx
+        # a hack to threshold values
+        alpha = np.array([i if np.abs(i) < 1e-10
+                         else (1e-10 if i > 0 else -1e-10) for i in alpha])
+        alpha2D = alpha if i == yrange[0] else np.vstack((alpha2D, alpha))
+    # fig, ax = plt.subplots()
+    # im = ax.imshow(alpha2D, cmap='seismic', interpolation='nearest',vmin=-9e-11, vmax=9e-11, origin='lower')
+    # divider = make_axes_locatable(ax)
+    # cax = divider.append_axes("right", size="5%", pad=0.05)
+    # plt.colorbar(im, cax=cax)
+    # plt.show()
+    X, Y = np.meshgrid(np.arange(0, x, 1), np.arange(yrange[0], yrange[1], 1))
+    fig2 = plt.figure()
+    ax2 = Axes3D(fig2)
+    surface = ax2.plot_surface(X*5, Y*5, alpha2D, rstride=1, cstride=1,
+                               cmap='seismic', vmin=-1e-10, vmax=1e-10)
+    fig2.colorbar(surface, shrink=0.8, pad=0.05)
+    # plt.show()
+
+
+# plot_rashba(file_path='./data/stray_field/rect/kjaergaard/strayfield_parallel6_kjaergaard.ovf', yslice=[118])
+# plot_rashba(file_path='./data/stray_field/rect/kjaergaard/strayfield_antiparallel6_kjaergaard.ovf', yslice=[128])
+# surface_alpha(file_path='./data/stray_field/halbach_switching/switch_study_perm_100_60/halbach_switch_perm.4.out/strayfield5.ovf', yrange=(125, 176))
+# surface_alpha(file_path='./data/stray_field/360_dw/strayfield_360wall5double.ovf', yrange=(15, 26))
+surface_alpha(file_path='./data/stray_field/rect/kjaergaard/strayfield_antiparallel6_kjaergaard.ovf', yrange=[128, 150])
--- plot_strayfield.py
+++ plot_strayfield.py
@@ -80,91 +80,40 @@ def compare_strayfields(base_path: str, thicknesses: [int], mag_dir: str):
     plt.show()
 
 
-def plot_rashba(file_path: str, yslice: [int]):
-    eff_m = 0.014
+def extract_maxfield(file_path: str, mag_dir: str, y_slice: int):
     x, y, z, _, _, _ = get_meta_data(file_path)
     print(x, y, z)
     zslice = int(z / 2)
     data = np.array(np.loadtxt(file_path))
     data_field = data.reshape(x, y, z, 3, order="F")
-    u, v, w = data_field[:, :, :, 0], data_field[:, :, :, 1], \
-        data_field[:, :, :, 2]
-    fig, ax1 = plt.subplots()
-    ax1.set_ylabel("alpha (eVm)")
-    ax2 = ax1.twinx()
-    ax2.set_ylabel("Magnetic Field (T)")
-    # ax1.set_ylim([1e-12, 5e-11])
-    # ax2.set_ylim([0, 0.07])
-    for i in yslice:
-        Bx = u[:, i, zslice]
-        By = v[:, i, zslice]
-        B = (u*u + v*v + w*w) ** 0.5
-        B = B[:, i, zslice]
-        grad_B = np.gradient(B)
-        grad_Bx = np.gradient(Bx)
-        # assumes in nanometres
-        dphi_dx = -(Bx * grad_B - B * grad_Bx)/(B*By*2*np.pi)
-        # alt expression but uses sqrt, might have sign issues?
-        dphi_dx2 = (Bx * grad_B - B * grad_Bx)/(2*np.pi*B*B*np.sqrt(1-Bx*Bx/(B*B)))
-        # extra 2pi factor from X = 2(pi)x
-        alpha = (3.818E-11/eff_m) * dphi_dx  # in eVm ## first factor hbar^2/2em, divide by e for eVs
-        alpha2 = (3.818E-11/eff_m) * dphi_dx2
-        ax1.plot([i * 5 for i in range(x)], alpha, label="alpha", color="blue")
-        # ax1.plot([i * 5 for i in range(x)], alpha2, color="orange", linestyle = "--")
-        # ax2.plot([i * 5 for i in range(x)], Bx,label= "$B_x$", color="brown")
-        # ax2.plot([i * 5 for i in range(x)], By,label="$B_y$", color="green")
-        ax2.plot([i * 5 for i in range(x)], B, label="$|B|$",  color="red")
-        # ax2.plot([i * 5 for i in range(x)], Bx/B, label="$Bx/|B|$",  color="red")
-        plt.legend()
-        plt.show()
-    return alpha
-
-
-def surface_alpha(file_path: str, yrange: tuple):
-    eff_m = 0.014
-    x, y, z, _, _, _ = get_meta_data(file_path)
-    print(x, y, z)
-    zslice = int(z / 2)
-    data = np.array(np.loadtxt(file_path))
-    data_field = data.reshape(x, y, z, 3, order="F")
-    u, v, w = data_field[:, :, :, 0], data_field[:, :, :, 1], \
-        data_field[:, :, :, 2]
-    for i in range(yrange[0], yrange[1]):
-        Bx = u[:, i, zslice]
-        By = v[:, i, zslice]
-        B = (u*u + v*v + w*w) ** 0.5
-        B = B[:, i, zslice]
-        grad_B = np.gradient(B)
-        grad_Bx = np.gradient(Bx)
-        dphi_dx = -(Bx * grad_B - B * grad_Bx)/(B*By*2*np.pi)
-        alpha = (3.818E-11/eff_m) * dphi_dx
-        # a hack to threshold values
-        alpha = np.array([i if np.abs(i) < 1e-10
-                         else (1e-10 if i > 0 else -1e-10) for i in alpha])
-        alpha2D = alpha if i == yrange[0] else np.vstack((alpha2D, alpha))
-    # fig, ax = plt.subplots()
-    # im = ax.imshow(alpha2D, cmap='seismic', interpolation='nearest',vmin=-9e-11, vmax=9e-11, origin='lower')
-    # divider = make_axes_locatable(ax)
-    # cax = divider.append_axes("right", size="5%", pad=0.05)
-    # plt.colorbar(im, cax=cax)
-    # plt.show()
-    X, Y = np.meshgrid(np.arange(0, x, 1), np.arange(yrange[0], yrange[1], 1))
-    fig2 = plt.figure()
-    ax2 = Axes3D(fig2)
-    surface = ax2.plot_surface(X*5, Y*5, alpha2D, rstride=1, cstride=1,
-                               cmap='seismic', vmin=-1e-10, vmax=1e-10)
-    fig2.colorbar(surface, shrink=0.8, pad=0.05)
-    # plt.show()
+    u, v, w = data_field[:,:,:,0], data_field[:,:,:,1], data_field[:,:,:,2]
+    if (mag_dir == 'x'):
+        mag = u
+    elif (mag_dir == 'y'):
+        mag = v
+    elif (mag_dir == 'z'):
+        mag = w
+    elif (mag_dir == 'total'):
+        mag = (u * u + v * v + w * w) ** 0.5
+    mag_slice = mag[:, yslice, zslice]
+    return max(mag_slice)
 
 
 # plot_strayfield(file_path='./data/stray_field/halbach_switching/switch_study_perm_100_60/halbach_switch_perm.4.out/strayfield5.ovf', mag_dir='total', yslice = [145,155,165])
+# plot_strayfield(file_path='./data/stray_field/halbach_switching/switch_study_perm_100_60/halbach_switch_perm.4.out/strayfield5.ovf', mag_dir='total', yslice = [145,155,165])
 # plot_strayfield(file_path='./data/stray_field/halbach_switching/switch_study_perm_100_60/halbach_switch_perm.4.out/strayfield5.ovf', mag_dir='x', yslice = [145,155,165])
 # plot_strayfield(file_path='./data/stray_field/halbach_switching/switch_study_perm_100_60/halbach_switch_perm.4.out/strayfield5.ovf', mag_dir='y', yslice = [145,155,165])
-# plot_rashba(file_path='./data/stray_field/rect/kjaergaard/strayfield_parallel6_kjaergaard.ovf', yslice=[118])
-# plot_rashba(file_path='./data/stray_field/rect/kjaergaard/strayfield_antiparallel6_kjaergaard.ovf', yslice=[128])
 # plot_strayfield(file_path='./data/stray_field/360_dw/strayfield_360wall9.ovf', mag_dir='y', yslice=[56, 64, 74])
-# surface_alpha(file_path='./data/stray_field/halbach_switching/switch_study_perm_100_60/halbach_switch_perm.4.out/strayfield5.ovf', yrange=(125, 176))
-# surface_alpha(file_path='./data/stray_field/360_dw/strayfield_360wall5double.ovf', yrange=(15, 26))
-surface_alpha(file_path='./data/stray_field/rect/kjaergaard/strayfield_antiparallel6_kjaergaard.ovf', yrange=[128, 150])
-plt.savefig('strayfield_antiparallel6_kjaergaard.pdf', dpi=1000)
-plt.show()
+
+all_B = []
+for i in range(20, 110, 10):
+    b_row = []
+    for j in range(0, 19):
+        B = extract_maxfield(file_path='../8array_sweeps/8array_sep' + str(i) + '/8array.' + str(j) + '.out/strayfield.ovf', mag_dir='total', y_slice=190)
+        b_row += [B]
+    all_B += b_row
+print(all_B)
+np.save(all_B)
+
+# plt.savefig('strayfield_antiparallel6_kjaergaard.pdf', dpi=1000)
+# plt.show()
