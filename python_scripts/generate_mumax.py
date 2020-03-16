d960155300fbeaba54db9a463d9bceb1d31800b4
ricktjwong <rickwtj@gmail.com>
2020-03-16 02:48:13 +0000

    Add sweeps for permalloy single

--- python_scripts/generate_mumax.py
+++ python_scripts/generate_mumax.py
@@ -2,10 +2,36 @@ import os
 import math
 
 
+def round_ten(x: int) -> int:
+    return int(math.ceil(x / 10.0)) * 10
+
+
+def generate_single_magnet(x: str, y: str, z: str, i: int):
+    lines = ''
+    lines += 'OutputFormat = OVF1_TEXT\n'
+    lines += 'MsPerm := 800e3\nExchangeEPerm := 13e-12\nAlphaPerm := 0.01\n'
+    lines += 'MsCo := 1400e3\nExchangeECo := 30e-12\nAlphaCo := 0.015\n'
+    lines += 'Ms := MsPerm\nExchangeE := ExchangeEPerm\nAlph := AlphaPerm\n'
+    lines += 'x := ' + x + '\n'
+    lines += 'y := ' + y + '\n'
+    lines += 'z := ' + z + '\n'
+    Nx = round_ten(float(x) / 5e-9)
+    Ny = round_ten((float(y) + 400e-9) / 5e-9)
+    Nz = math.ceil(float(z) / 5e-9)
+    lines += 'SetCellsize(5e-9, 5e-9, 5e-9)\n'
+    lines += 'SetGridsize(' + str(Nx) + ', ' + str(Ny) + ', ' + str(Nz) + ')\n'
+    with open('perm_single.' + str(i) + '.mx3', 'w') as f1:
+        for line in lines:
+            f1.write(line)
+        with open('single_boilerplate.mx3') as f:
+            for line in f:
+                f1.write(line)
+
+
 def generate_parallel_magnets(x: str, y: str, z: str, s: str, i: int):
     lines = ''
     lines += 'OutputFormat = OVF1_TEXT\n'
-    lines += 'MsPerm := 800e3\nExchangeEPerm := 13e-10\nAlphaPerm := 0.01\n'
+    lines += 'MsPerm := 800e3\nExchangeEPerm := 13e-12\nAlphaPerm := 0.01\n'
     lines += 'MsCo := 1400e3\nExchangeECo := 30e-12\nAlphaCo := 0.015\n'
     lines += 'Ms := MsPerm\nExchangeE := ExchangeEPerm\nAlph := AlphaPerm\n'
     lines += 'x := ' + x + '\n'
@@ -13,8 +39,8 @@ def generate_parallel_magnets(x: str, y: str, z: str, s: str, i: int):
     lines += 'z := ' + z + '\n'
     lines += 's := ' + s + '\n'
     lines += 'SetCellsize(5e-9, 5e-9, 5e-9)\n'
-    Nx = math.ceil((float(x) * 8 + float(s) * 7) / 5e-9)
-    Ny = math.ceil((float(y) + 400e-9) / 5e-9)
+    Nx = round_ten((float(x) * 8 + float(s) * 7) / 5e-9)
+    Ny = round_ten((float(y) + 400e-9) / 5e-9)
     Nz = math.ceil(float(z) / 5e-9)
     lines += 'SetGridsize(' + str(Nx) + ', ' + str(Ny) + ', ' + str(Nz) + ')\n'
     with open('sweep_width_sep_' + str(math.ceil(float(s)/1e-9)) +
@@ -55,17 +81,17 @@ def generate_halbach_arrays(h_length: str, v_width: str, i: int):
                 f1.write(line)
 
 
-sweep_sep = [i for i in range(20, 110, 10)]
-sweep_width = [i for i in range(20, 620, 20)]
-for j in range(len(sweep_sep)):
-    if not os.path.exists('sweep_width_sep_' + str(sweep_sep[j])):
-        os.makedirs('sweep_width_sep_' + str(sweep_sep[j]))
-    for i in range(len(sweep_width)):
-        generate_parallel_magnets(x=str(sweep_width[i]) + 'e-9',
-                                  y='600e-9',
-                                  z='20e-9',
-                                  s=str(sweep_sep[j]) + 'e-9',
-                                  i=i)
+# sweep_sep = [i for i in range(20, 110, 10)]
+# sweep_width = [i for i in range(20, 620, 20)]
+# for j in range(len(sweep_sep)):
+#     if not os.path.exists('sweep_width_sep_' + str(sweep_sep[j])):
+#         os.makedirs('sweep_width_sep_' + str(sweep_sep[j]))
+#     for i in range(len(sweep_width)):
+#         generate_parallel_magnets(x=str(sweep_width[i]) + 'e-9',
+#                                   y='600e-9',
+#                                   z='20e-9',
+#                                   s=str(sweep_sep[j]) + 'e-9',
+#                                   i=i)
 
 
 # sweep_vwidth = [i for i in range(40, 220, 20)]
@@ -77,3 +103,11 @@ for j in range(len(sweep_sep)):
 #         generate_halbach_arrays(h_length=str(sweep_hlength[i]) + 'e-9',
 #                                 v_width=str(sweep_vwidth[j]) + 'e-9',
 #                                 i=i)
+
+
+x = [i for i in range(10, 610, 10)]
+for i in range(len(x)):
+    generate_single_magnet(x=str(x[i])+'e-9',
+                           y='600e-9',
+                           z='100e-9',
+                           i=i)
